6d7fc505b93f47d1a4b1bcda5ade5dee9d57a2f8
Huidae Cho <grass4u@gmail.com>
2025-06-13 07:41:42 -0600

    Fix output extension for Windows

--- test/test.bat
+++ test/test.bat
@@ -6,5 +6,5 @@ set PROJ_LIB=..\windows\lib
 for %%o in (inputs\outlets*.shp) do (
 	set o=%%~no
 	set l=!o:outlets=lfp!
-	melfp -l inputs\fdr.tif %%o cat outputs\!l!.tif
+	melfp -l inputs\fdr.tif %%o cat outputs\!l!.txt
 )
